eb1e6b1caedc5e0d971052033425477ffe328e56
hualeel <hualeel@sina.com>
2020-02-20 18:22:22 +0800

    加入api上报功能

--- src/main/java/com/hua/tsf_spring_cloud_demo/tsf_spring_cloud_consumer/Controller/EchoController.java
+++ src/main/java/com/hua/tsf_spring_cloud_demo/tsf_spring_cloud_consumer/Controller/EchoController.java
@@ -1,6 +1,7 @@
 package com.hua.tsf_spring_cloud_demo.tsf_spring_cloud_consumer.Controller;
 
 import com.hua.tsf_spring_cloud_demo.tsf_spring_cloud_consumer.Interface.EchoService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.concurrent.ListenableFuture;
@@ -22,12 +23,14 @@ public class EchoController {
 
 //    /echo-rest/
     @RequestMapping(value = "/echo-rest/{str}", method = RequestMethod.GET)
+    @ApiOperation(value= "/echo-rest/{str}", notes = "consumer微服务，调用provider微服务，进行静态方式回显字符串，") // notes 对应 API 描述
     public String rest(@PathVariable String str) {
         return restTemplate.getForObject("http://tsf-provider/echo/" + str, String.class);
     }
 
 //    /echo-async-rest/
     @RequestMapping(value = "/echo-async-rest/{str}", method = RequestMethod.GET)
+    @ApiOperation(value= "/echo-rest/{str}", notes = "consumer微服务，调用provider微服务，进行动态方式回显字符串，") // notes 对应 API 描述
     public String asyncRest(@PathVariable String str) throws Exception {
         ListenableFuture<ResponseEntity<String>> future = asyncRestTemplate.getForEntity("http://tsf-provider/echo/" + str, String.class);
         return future.get().getBody();
@@ -35,6 +38,7 @@ public class EchoController {
 
 //    /echo-feign/
     @RequestMapping(value = "/echo-feign/{str}", method = RequestMethod.GET)
+    @ApiOperation(value= "/echo-rest/{str}", notes = "consumer微服务，调用provider微服务，进行feign方式回显字符串，") // notes 对应 API 描述
     public String feign(@PathVariable String str) {
         return echoService.echo(str);
     }
