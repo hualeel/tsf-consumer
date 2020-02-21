5992d3a06e7c77027585ffb503d8929572ba8f27
hualeel <hualeel@sina.com>
2020-02-21 15:47:17 +0800

    增加熔断，自定义标签，限流，路由等功能

--- src/main/java/com/hua/tsf_spring_cloud_demo/tsf_spring_cloud_consumer/Controller/EchoController.java
+++ src/main/java/com/hua/tsf_spring_cloud_demo/tsf_spring_cloud_consumer/Controller/EchoController.java
@@ -1,14 +1,14 @@
 package com.hua.tsf_spring_cloud_demo.tsf_spring_cloud_consumer.Controller;
 
+import com.hua.tsf_spring_cloud_demo.tsf_spring_cloud_consumer.Entity.CustomMetadata;
 import com.hua.tsf_spring_cloud_demo.tsf_spring_cloud_consumer.Interface.EchoService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.tsf.core.TsfContext;
+import org.springframework.util.StringUtils;
 import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,24 +21,33 @@ public class EchoController {
     @Autowired
     private EchoService echoService;
 
-//    /echo-rest/
+    //    /echo-rest/
     @RequestMapping(value = "/echo-rest/{str}", method = RequestMethod.GET)
-    @ApiOperation(value= "/echo-rest/{str}", notes = "consumer微服务，调用provider微服务，进行静态方式回显字符串，") // notes 对应 API 描述
-    public String rest(@PathVariable String str) {
+//    @ApiOperation(value = "/echo-rest/{str}", notes = "consumer微服务，调用provider微服务，进行静态方式回显字符串，") // notes 对应 API 描述
+    public String rest(@PathVariable String str,
+                       @RequestParam(required = false) String tagName,
+                       @RequestParam(required = false) String tagValue) {
+
+//        通过url传参设置自定义标签
+        if (!StringUtils.isEmpty(tagName)) {
+            TsfContext.putTag(tagName, tagValue);
+            TsfContext.putCustomMetadata(new CustomMetadata(tagName, tagValue));
+        }
+
         return restTemplate.getForObject("http://tsf-provider/echo/" + str, String.class);
     }
 
-//    /echo-async-rest/
+    //    /echo-async-rest/
     @RequestMapping(value = "/echo-async-rest/{str}", method = RequestMethod.GET)
-    @ApiOperation(value= "/echo-rest/{str}", notes = "consumer微服务，调用provider微服务，进行动态方式回显字符串，") // notes 对应 API 描述
+//    @ApiOperation(value = "/echo-async-rest/{str}", notes = "consumer微服务，调用provider微服务，进行动态方式回显字符串，") // notes 对应 API 描述
     public String asyncRest(@PathVariable String str) throws Exception {
         ListenableFuture<ResponseEntity<String>> future = asyncRestTemplate.getForEntity("http://tsf-provider/echo/" + str, String.class);
         return future.get().getBody();
     }
 
-//    /echo-feign/
+    //    /echo-feign/
     @RequestMapping(value = "/echo-feign/{str}", method = RequestMethod.GET)
-    @ApiOperation(value= "/echo-rest/{str}", notes = "consumer微服务，调用provider微服务，进行feign方式回显字符串，") // notes 对应 API 描述
+//    @ApiOperation(value = "/echo-feign/{str}", notes = "consumer微服务，调用provider微服务，进行feign方式回显字符串，") // notes 对应 API 描述
     public String feign(@PathVariable String str) {
         return echoService.echo(str);
     }
--- src/main/java/com/hua/tsf_spring_cloud_demo/tsf_spring_cloud_consumer/TsfSpringCloudConsumerApplication.java
+++ src/main/java/com/hua/tsf_spring_cloud_demo/tsf_spring_cloud_consumer/TsfSpringCloudConsumerApplication.java
@@ -6,12 +6,15 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.tsf.annotation.EnableTsf;
+import org.springframework.tsf.auth.annotation.EnableTsfAuth;
 import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients // 使用 Feign 微服务调用时请启用
 @EnableTsf
+//服务鉴权
+@EnableTsfAuth
 public class TsfSpringCloudConsumerApplication {
 
     @LoadBalanced
